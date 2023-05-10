4d9c9640b0398384ff06d9f1114bec774da8efc4
jemba narcisse <cabliveer@gmail.com>
2023-05-10 09:19:23 +0200

    Check if data is added into local storage

--- __test__/addNewTask.test.js
+++ __test__/addNewTask.test.js
@@ -1,3 +1,26 @@
-it("", () => {
+import { LocalStorageMock } from "../src/modules/localStorageMock";
+import { addNewTask } from "../src/utils";
+
+global.localStorage = new LocalStorageMock;
+
+describe("Add item function", () => {
+
+    let data = [];
+        const task = {
+            description: "task 1",
+            completed: false,
+            index: 1,
+    }
+
+    it("data is added into local storage", () => {
+         
+        addNewTask(task, data)
     
+        expect(JSON.parse(localStorage.getItem('list'))).toEqual([task])
+    
+            })
+
+     
+
 })
+

c3bf664b7613f4a37d9f41af7c32d2820560066e
jemba narcisse <cabliveer@gmail.com>
2023-05-12 05:46:54 +0200

    Solve linter eslint errors

--- src/__test__/editTask.test.js
+++ src/__test__/editTask.test.js
@@ -1,66 +1,57 @@
-import { addNewTask, editTask } from "../utils";
+import { addNewTask, editTask } from '../utils.js';
 import display from '../__mocks__/indexMock.js';
 
-describe("Edit item description function", () => {
+describe('Edit item description function', () => {
+  it('Edit unique item and check Dom', () => {
+    const task1 = {
+      description: 'task 1',
+      completed: false,
+      index: 1,
+    };
 
-    it("Edit unique item and check Dom", () => {
+    addNewTask(task1);
 
-        const task1 = {
-            description: 'task 1',
-            completed: false,
-            index: 1,
-          };
-      
-        addNewTask(task1);
-    
-        display();
+    display();
 
-        const input = document.querySelector('.value');
-        
-        // console.log(input.value)
-        // input.click();
-        
-        input.value = "update task 1"
+    const input = document.querySelector('.value');
 
-        editTask(input)
+    // console.log(input.value)
+    // input.click();
 
-        display()
+    input.value = 'update task 1';
 
-        expect(document.querySelector('.value').value).toBe("update task 1")
+    editTask(input);
 
+    display();
 
-    })
+    expect(document.querySelector('.value').value).toBe('update task 1');
+  });
 
-    it("Edit one item among 10 and check Dom", () => {
+  it('Edit one item among 10 and check Dom', () => {
+    [...Array(10).keys()].map((i) => i + 1).forEach((v) => addNewTask(
+      {
+        description: `task ${v}`,
+        completed: false,
+        index: v + 1,
+      },
+    ));
 
-        [...Array(10).keys()].map((i) => i + 1).forEach((v) => addNewTask(
-            {
-              description: `task ${v}`,
-              completed: false,
-              index: v + 1,
-            },
-          ));
-    
-        display();
+    display();
 
-        const input = document.querySelectorAll('.value');
-        
-        // input[8].click();
-        
-        input[8].value = "update task 1"
+    const input = document.querySelectorAll('.value');
 
-        editTask(input[8])
+    // input[8].click();
 
-        console.log(input[8].parentElement.parentElement.id)
+    input[8].value = 'update task 1';
 
-        display()
+    editTask(input[8]);
 
-         expect(document.querySelectorAll('.value')[8].value).toBe("update task 1")
+    display();
 
+    expect(document.querySelectorAll('.value')[8].value).toBe('update task 1');
+  });
 
-    })
-
-    it('Check if description is saved into local storage', () => {
-        expect(JSON.parse(localStorage.getItem('list'))[8].description).toBe("update task 1");
-      });
-})
+  it('Check if description is saved into local storage', () => {
+    expect(JSON.parse(localStorage.getItem('list'))[8].description).toBe('update task 1');
+  });
+});
